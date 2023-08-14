c8037156a603c92e4aa17b84facba9a04c32b8f0
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2023-08-14 21:31:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,12 +35,11 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
 @app.get("/paper/{keyword}")
